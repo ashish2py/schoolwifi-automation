f6b57837e4562e75e7858e0df465237bbc5fc812
Ashish T <ashish2py@gmail.com>
2018-08-02 13:35:48 +0530

    Page loop and media check added.

--- englishduniya.py
+++ englishduniya.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import sys
+import requests
 
 
 class SchoolWifiTestSuite(unittest.TestCase):
@@ -14,12 +15,14 @@ class SchoolWifiTestSuite(unittest.TestCase):
 
     # timer/sleep with counter
     def let_me_sleep(self, time):
-        for counter in range(time):
-            sys.stdout.write("\rWAITING FOR {}/{}  :: ".format(counter + 1, time))
-            sys.stdout.flush()
-            sleep(counter)
-        print ('\n')
+        # for counter in range(time):
+            # sys.stdout.write("\rWAITING FOR {}/{}  :: ".format(counter + 1, time))
+            # sys.stdout.flush()
+            # sleep(counter)
+        sleep(time)
+        # print ('\n')
 
+    # paginaction
     def calculate_page(self):
         pagination_pages = self.driver.find_elements_by_css_selector('.pagination-page.ng-scope')
         # print('PAGES #{}'.format(pagination_pages[-1].text))
@@ -33,22 +36,86 @@ class SchoolWifiTestSuite(unittest.TestCase):
             return next_button
         return False
 
-    def click_next_button(self):
+    def click_next_button_and_play_video(self):
         number_of_pages = self.calculate_page()
         number_of_pages = int(number_of_pages[-1].text) - 1
         print (number_of_pages)
         for page in range(number_of_pages):
-            print ('PAGE #',page+1)
+            print ('\n\nPAGE #', page + 1)
             next_button_active = self.check_next_button_active()
             if next_button_active:
-                self.let_me_sleep(3)
-                next_button_active.click()
+                played_batch = self.list_and_play_videos()
+                if played_batch:
+                    next_button_active = self.check_next_button_active()
+                    next_button_active.click()
+                else:
+                    print ('FAILED while playing video in pagination')
             continue
+    # end of paginaction
 
+    # media files
+    def get_media_list(self):
+        ''' CALCULATE MEDIA LIST on iframe'''
+        self.calculate_page()
+        list_elements = self.driver.find_elements_by_css_selector('.media.pointer.ng-scope')
+        return list_elements
+
+    def open_media_content(self, counter):
+        '''
+        MEDIA CONTENT CHECK AVAILABILITY
+        1. click on media content list.
+        2. look for VIDEO tag and check for SRC attribute.
+        3. check media files availability.
+        4. if not available then log into error log and return false.
+        5. if availabel then log it as a succesfull.  '''
+        self.let_me_sleep(2)
+        print ('\n')
+
+        # 1
+        element = self.driver.find_element_by_xpath('/html/body/div/div/div[1]/div/div/\
+                                                     div[2]/div/div/ul/li[{}]'.format(counter + 1))
+        print('PLAYING #{}/{}'.format(counter + 1, element.text))
+        element.click()
+        self.let_me_sleep(3)
+
+        # 2
+        # check media file availability by making HEADE request to media file.
+        video_url = self.driver.find_element_by_tag_name('video').get_attribute('src')
+
+        # 3
+        request = requests.head(video_url)
+        if request.status_code == 200:
+            # 4 TODO : log success
+            print ('SIZE ', request.headers.get('Content-Length'))
+            print('SUCCESS : media file is available')
+        else:
+            # 5 TODO : log faiure and return FALSE
+            print('ERROR : media file is unavailable')
+
+        # Go Back
+        self.driver.find_element_by_css_selector('.btn.btn-danger').click()
+
+    def list_and_play_videos(self):
+        list_media_files = self.get_media_list()
+        len_elements = len(list_media_files)
+        print ('MEDIA FILES TO BE TESTED #', str(len_elements))
+
+        # open list
+        try:
+            for key, source in enumerate(list_media_files):
+                self.open_media_content(key)
+                self.let_me_sleep(2)
+            return True
+        except Exception as e:
+            print('Exception occurs' + str(e))
+            return False
+    # end of media files
+
+    # test case
     def test_01_englishduniya(self):
-        self.driver.get("http://sw-englishduniya.zaya.in/")
+        self.driver.get("http://sws-englishduniya.zaya.in/")
         sleep(5)
-        self.click_next_button()
+        self.click_next_button_and_play_video()
 
     def tearDown(self):
         self.driver.close()
--- htmlvideo.py
+++ htmlvideo.py
@@ -1,15 +1,19 @@
-from time import sleep
-import unittest
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+# Copyright 2008-2009 WebDriver committers
+# Copyright 2008-2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from selenium.common.exceptions import StaleElementReferenceException
-
-import sys
-import requests
-
-
 
 def stale(fn):
     def wrapper(self, *args):
@@ -384,233 +388,40 @@ class Audio(HTML5Media):
         self._el = webelement
         self.changed = False
 
-
-class PythonOrgSearch(unittest.TestCase):
-
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("http://wifi.zaya.in/#/dashboard")
-        self.webserver = 'http://wifi.zaya.in/#/dashboard'
-
-    # timer/sleep with counter
-    def let_me_sleep(self, time):
-        for counter in range(time):
-            sys.stdout.write("\rWAITING FOR {}/{}  :: ".format(counter + 1, time))
-            sys.stdout.flush()
-            sleep(counter)
-        print ('\n')
-
-    # swtich to iframe content
-    def switch_to_iframe_by_xpath(self, element_key):
-        self.driver.switch_to.frame(self.driver.find_element_by_xpath(element_key))
-
-    # swtich to default content
-    def switch_to_default_content(self):
-        self.driver.switch_to.default_content()
-
-    # make head request and prepare a passed and failed requests
-    def head_request_report(self, requests):
-        pass
-
-    def get_current_url(self):
-        return self.driver.get_current_url
-
-    def check_video(self):
-        pass
-
-    ##############################
-    def get_media_list(self):
-        ''' CALCULATE MEDIA LIST on iframe'''
-        self.calculate_page()
-        list_elements = self.driver.find_elements_by_css_selector('.media.pointer.ng-scope')
-        return list_elements
-
-    def open_media_content(self, counter):
-        '''
-        MEDIA CONTENT CHECK AVAILABILITY
-        1. click on media content list.
-        2. look for VIDEO tag and check for SRC attribute.
-        3. check media files availability.
-        4. if not available then log into error log and return false.
-        5. if availabel then log it as a succesfull.  '''
-        self.let_me_sleep(2)
-
-        # 1
-        element = self.driver.find_element_by_xpath('/html/body/div/div/div[1]/div/div/\
-                                                     div[2]/div/div/ul/li[{}]'.format(counter + 1))
-        print('PLAYING #{}/{}'.format(counter, element.text))
-        element.click()
-        self.let_me_sleep(3)
-
-        # 2
-        # check media file availability by making HEADE request to media file.
-        video_url = self.driver.find_element_by_tag_name('video').get_attribute('src')
-
-        # 3
-        request = requests.head(video_url)
-        if request.status_code == 200:
-            # 4 TODO : log success
-            print ('SIZE ', request.headers.get('Content-Length'))
-            print('SUCCESS : media file is available')
-        else:
-            # 5 TODO : log faiure and return FALSE
-            print('ERROR : media file is unavailable')
-
-        # Go Back
-        self.driver.find_element_by_css_selector('.btn.btn-danger').click()
-
-    ################################################
-    #  PAGINATION
-    #  TODO : Better way to handle pagination
-    ################################################
-    def calculate_page(self):
-        pagination_pages = self.driver.find_elements_by_css_selector('.pagination-page.ng-scope')
-        # print('PAGES #{}'.format(pagination_pages[-1].text))
-        return pagination_pages
-
-    def check_next_button_active(self):
-        if not self.driver.find_elements_by_css_selector('.pagination-next.ng-scope.disabled'):
-            next_button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
-                (By.XPATH, '/html/body/div/div/div[1]/div/div/div[3]/ul/li[9]/a')))
-            return next_button
-        return False
-
-    # JUGAAD
-    def multiple_button_click(self, counter, item):
-        self.let_me_sleep(2)
-        for click in range(counter):
-            item.click()
-
-    def click_next_button(self):
-        import ipdb;ipdb.set_trace()
-        number_of_pages = self.calculate_page()
-
-        for index, page in enumerate(number_of_pages):
-            next_button_active = self.check_next_button_active()
-            if next_button_active:
-                self.multiple_button_click(index + 1, next_button_active)
-            continue
-
-    ##############################
-    #   FILTERS
-    ##############################
-    def searchbar_filter_check(self):
-        ''' SEND input to searchbar '''
-        search_bar = self.driver.find_elements_by_css_selector(
-            '.form-control.ng-pristine.ng-valid.ng-empty.ng-touched')
-        if search_bar:
-            search_bar[0].click()
-            search_bar[0].send_keys('Grammar')
-            self.let_me_sleep(2)
-            search_result = self.driver.find_elements_by_css_selector('.media-heading.ng-binding')
-            self.assertTrue('Grammar' in search_result[0].text)
-
-    def grade_filter(self):
-        pass
-
-    def skill_filter(self):
-        pass
-
-    def title_filter(self):
-        pass
-
-    def check_filters(self):
-        ''' Search filter '''
-        tbody_available = self.driver.find_elements_by_xpath(
-            '/html/body/div/div/div[1]/div/div/div[1]/div/div/div[2]/table/tbody')
-        if tbody_available:
-            tbody = tbody_available[0]
-            tbody_trs = tbody.find_elements_by_tag_name('tr')
-            for tr in tbody_trs:
-                tr_tds = tr.find_elements_by_tag_name('td')
-                for td in tr_tds:
-                    # print(td.text)
-                    # TODO : Add id/class to input-search-bar or name to avoid string comparision
-                    # SEARCH Filter
-                    if td.text is '':
-                        # print('SEARCH FILTER')
-                        td.click()
-                        # click search bar
-                        self.driver.find_element_by_css_selector(
-                            '.form-control.ng-pristine.ng-valid.ng-empty.ng-untouched').click()
-                        self.let_me_sleep(2)
-                        self.searchbar_filter_check()
-
-                    # TODO : Grade filter
-                    # TODO : Skill filter
-        else:
-            print('SEARCH FIELD NOT AVAILABLE')
-
-    ##############################
-    # FILTERS
-    ##############################
-
-    # @unittest.skip("zaya apps")
-    def test_000_zaya_applications(self):
-        print('---------------------------------------------------------------------------------------------')
-        print('TESTING ZAYA APP OVER {}. MAKE SURE YOUR ARE CONNECTED TO BOX/INTERNET!!'.format(self.webserver))
-        print('---------------------------------------------------------------------------------------------')
-        self.driver.get(self.webserver)     # add your webserver/website name
-        self.let_me_sleep(6)
-
-        application_elements = self.driver.find_elements_by_class_name('app-block')
-        application_count = len(application_elements) + 1
-        for app_counter in range(application_count):
-            sleep(2)
-            _application_elements = self.driver.find_elements_by_class_name('app-block')
-            _application_elements[app_counter].click()
-            _application_name = self.driver.find_element_by_class_name('page-title')
-            print ('APP TESTING #', _application_name.text)
-
-            # check availability of iframe and switch
-            iframe_available = self.driver.find_elements_by_xpath('//*[@id="loaded"]')
-            if iframe_available:
-                self.switch_to_iframe_by_xpath('//*[@id="loaded"]')
-            else:
-                print('APPLICATION TAKING TIME TO LOAD')
-                continue
-
-            # test filters
-            # self.check_filters()
-
-            self.click_next_button() 
-
-            # list_elements = self.get_media_list()
-            # len_elements = len(list_elements)
-            # print ('MEDIA FILES TO BE TESTED #', str(len_elements))
-
-            # # open list
-            # try:
-            #     for key, source in enumerate(list_elements):
-            #         self.open_media_content(key)
-            #         self.let_me_sleep(3)
-            #         if key + 1 == len_elements:
-            #             self.switch_to_default_content()
-            # except Exception as e:
-            #     print('Exception occurs' + str(e))
-            #     pass
-
-            self.switch_to_default_content()
-
-    def tearDown(self):
-        self.driver.close()
-
-    # def test_111_switch_applications(self):
-    #     sleep(5)
-    #     application_elements = self.driver.find_elements_by_class_name('app-block')
-    #     application_count = len(application_elements) + 1
-    #     for app_counter in range(application_count):
-    #         sleep(2)
-    #         _application_elements = self.driver.find_elements_by_class_name('app-block')
-    #         _application_elements[app_counter].click()
-    #         _application_name = self.driver.find_element_by_class_name('page-title')
-    #         print ('APP TESTING #', _application_name.text)
-    #         sleep(4)
-    #         self.goBack()
-
-    def goBack(self):
-        self.driver.execute_script("window.history.go(-1)")
-
-
 if __name__ == "__main__":
-    unittest.main()
+    from selenium import webdriver
+    
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.w3schools.com/tags/movie.mp4")
+    e = driver.find_element_by_css_selector("video")
+    v = Video(e)
+    v.play()
+
+    import time
+    time.sleep(5)
+
+    v.pause()
+    v.width = 600
+    e = driver.find_element_by_css_selector("video")
+    v = Video(e)
+    print("Width: {0}".format(v.width))
+    v.height = 500
+    e = driver.find_element_by_css_selector("video")
+    v = Video(e)
+    print("Height: {0}".format(v.height))
+    print("Current Source: {0}".format(v.current_source))
+    print("Network State: {0}".format(v.network_state))
+    print("Ready State: {0}".format(v.ready_state))
+    print("Buffered: {0}".format(v.buffered))
+    print("Can Play Type? (video/ogg): {0}".format(v.can_play_type("video/ogg")))
+    print("Can Play Type? (video/mp4): {0}".format(v.can_play_type("video/mp4")))
+    print("Can Play Type? (video/webm): {0}".format(v.can_play_type("video/webm")))
+    print("Seeking: {0}".format(v.seeking))
+    print("Duration: {0}".format(v.duration))
+    print("Paused?: {0}".format(v.paused))
+    print("Played: {0}".format(v.played))
+    print("Seekable: {0}".format(v.seekable))
+    print("Ended?: {0}".format(v.ended))
+
+    driver.quit()
